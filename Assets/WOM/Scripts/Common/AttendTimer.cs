d684ad9449ce29337bd99837e9ef1bf54e3f4318
freeboxwork <33273908+freeboxwork@users.noreply.github.com>
2023-06-12 18:28:25 +0900

    Update AttendTimer.cs

--- Assets/WOM/Scripts/Common/AttendTimer.cs
+++ Assets/WOM/Scripts/Common/AttendTimer.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AttendTimer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public string attendTestDateValue = "2023-06-12";
+    const string LAST_ATTENDANCE_DATE_KEY = "last_attendance_date";
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public bool HasLastAttendanceDate()
+    {
+        return PlayerPrefs.HasKey(LAST_ATTENDANCE_DATE_KEY);
     }
 }

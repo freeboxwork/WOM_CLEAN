19053e44fcb3dd21e7d55e5b96375d8bed9720f8
freeboxwork <33273908+freeboxwork@users.noreply.github.com>
2023-07-01 12:37:25 +0900

    건의 이메일 보내기 기능

---
+++ Assets/WOM/Scripts/Controller/MainToDonnieNest.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainToDonnieNest : MonoBehaviour
+{
+
+    public Button btnEmail;
+
+
+    void Start()
+    {
+        AddBtnEvnet();
+    }
+
+    void AddBtnEvnet()
+    {
+        btnEmail.onClick.AddListener(OnClickEvnet);
+    }
+
+    public void OnClickEvnet()
+    {
+        string Emailadd = "donnie127834@gmail.com";
+        string Emailname = System.Uri.EscapeDataString("버그 리포트 / 문의사항");
+
+        string body = $"내용을 작성해주세요 : \n\n\n\nDevice Model : {SystemInfo.deviceModel} \nDevice OS : {SystemInfo.operatingSystem} \n\n";
+        string Emailbody = System.Uri.EscapeDataString
+            (
+             body
+            );
+
+        string mailtoUrl = string.Format("mailto:{0}?subject={1}&body={2}", Emailadd, Emailname, Emailbody);
+        //Application.OpenURL("mailto:" + Emailadd + "?subject=" + Emailname + "&body=" + Emailbody);
+        Application.OpenURL(mailtoUrl);
+    }
+
+    // [System.Obsolete]
+    // private string EscapeURL(string url)
+    // {
+    //     return WWW.EscapeURL(url).Replace("+", "%20");
+    // }
+}

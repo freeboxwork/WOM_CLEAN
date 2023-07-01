ade4e3fb2c453ad3d79c0e7a3eede61194e6c6a7
freeboxwork <33273908+freeboxwork@users.noreply.github.com>
2023-07-01 16:54:16 +0900

    앱 업데이트 패키지 , 업데이트 체크및 실행 로직 추가

---
+++ Assets/WOM/Scripts/Manager/WOMAppUpdateManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using UnityEngine;
+using Google.Play.Common;
+using Google.Play.AppUpdate;
+
+
+
+public class WOMAppUpdateManager : MonoBehaviour
+{
+
+
+    void Start()
+    {
+
+#if UNITY_ANDROID
+        StartCoroutine(AppUpdateCheck());
+#endif
+    }
+
+
+    public IEnumerator AppUpdateCheck()
+    {
+        // 매니저 정의
+        AppUpdateManager appUpdateManager = new AppUpdateManager();
+        // 업데이트 체크
+        PlayAsyncOperation<AppUpdateInfo, AppUpdateErrorCode> appUpdateInfoTask = appUpdateManager.GetAppUpdateInfo();
+        // 업데이트 체크 완료까지 기다림
+        yield return appUpdateInfoTask;
+        // 업데이트 정보를 성공적으로 받아옴
+        if (appUpdateInfoTask.IsSuccessful)
+        {
+            AppUpdateInfo appUpdateResult = appUpdateInfoTask.GetResult();
+            // 업데이트가 필요한 경우
+            if (appUpdateResult.UpdateAvailability == UpdateAvailability.UpdateAvailable)
+            {
+                // 업데이트를 시작
+                var appUpdateOption = AppUpdateOptions.ImmediateAppUpdateOptions();
+                var startUpdateReq = appUpdateManager.StartUpdate(appUpdateResult, appUpdateOption);
+                // 업데이트 완료까지 기다림
+                yield return startUpdateReq;
+                // 업데이트가 성공적으로 완료됨
+                if (startUpdateReq.IsDone)
+                {
+                    // 업데이트 완료
+                    Debug.Log("APP 업데이트 완료");
+
+                }
+                // 업데이트가 실패한 경우
+                else
+                {
+                    // 업데이트 실패
+                    Debug.Log("APP 업데이트 실패");
+                }
+            }
+            // 업데이트가 필요하지 않은 경우
+            else
+            {
+                // 업데이트 필요 없음
+                Debug.Log("APP 업데이트 필요하지 않음");
+            }
+        }
+        else
+        {
+            Debug.Log("APP 업데이트 정보 받기 실패");
+        }
+
+    }
+
+
+}

2b6d2fd8e8c12426b65c5bb3be3b248bbfab8062
centuruk <63578897+centuruk@users.noreply.github.com>
2023-07-11 18:38:55 +0900

    인트로 시네마틱 프레임 점프 시스템.

---
+++ Assets/WOM/Scripts/Graphics/IntroTimeLineController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+
+public class IntroTimeLineController : MonoBehaviour
+{
+    public PlayableDirector director;
+    public double[] cutFrames;
+    public int currentTimeIndex = 0;
+
+    [Range(0.0f, 2.0f)]
+    public float deleyTime = 0.8f;
+    private bool dontTouch = false;
+    
+    private void OnEnable()
+    {
+        director.initialTime = cutFrames[currentTimeIndex = 0];
+        director.Play();
+    }
+
+    float t = 0.0f;
+    void Update()
+    {
+        if (!dontTouch && currentTimeIndex < (cutFrames.Length -1))
+        {
+            if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.P))
+            {
+                double currentTime = director.time;
+                for (int i = 0; i < cutFrames.Length; i++)
+                {
+                    if(currentTime <= cutFrames[i])
+                    {
+                        currentTimeIndex = i;
+                        break;
+                    }
+                }
+                director.time = cutFrames[currentTimeIndex];
+                dontTouch = true;
+            }
+        }
+        else
+        {
+            t += Time.deltaTime;
+            if(t >= deleyTime)
+            {
+                t = 0.0f;
+                dontTouch = false;
+            }
+        }
+    }
+
+    public void OnMarkableIntroSignal()
+    {
+        Debug.Log("시그날 왔다구리");
+    }
+
+
+    private void OnDisable()
+    {
+        currentTimeIndex = 0;
+        director.Stop();    
+    }
+}

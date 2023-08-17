ad2eac063a9d678d0a669e164173253798ab67f4
centuruk <63578897+centuruk@users.noreply.github.com>
2023-08-17 19:32:28 +0900

    아이콘 애니메이션

--- Assets/WOM/Scripts/Graphics/UI/IconScale.cs
+++ Assets/WOM/Scripts/Graphics/UI/IconScale.cs
@@ -1,19 +1,47 @@
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class IconScale : MonoBehaviour
 {
-    public float powValue = 1;
+
     public RectTransform rectTransform;
-    [Header("x 아이콘 크기 배수"), Range(0.5f, 2)]
+    [Header("x 아이콘 크기 배수"), Range(0.0f, 2)]
     public float multiscale = 1;
     [Range(0, 3)]
     public float timimg = 1;
-    private float pingpong = 0;
-    // Update is called once per frame
-    void Update()
+    private float p = 0;
+
+    public AnimationCurve actionCurve;
+
+    private bool isDelay = true;
+    [Header("액션 대기")]
+    public float delayTime = 1.0f;
+    private float dt = 0.0f;
+
+    private void Start()
     {
-        var time = Mathf.Pow(Time.time, powValue);
-        pingpong = (Mathf.PingPong((Time.time * timimg), 1) + 1) * multiscale;
-        rectTransform.localScale = new Vector3(pingpong, pingpong, pingpong);
+        isDelay = true;
+    }
+
+    void FixedUpdate()
+    {
+        dt += Time.deltaTime;
+        if (dt >= delayTime) isDelay = false;
+
+        if (isDelay) return;
+
+        p += Time.deltaTime;
+        float comparePoint = Mathf.Clamp01(p * timimg);
+        float value = actionCurve.Evaluate(comparePoint) * multiscale;
+        value++;
+        rectTransform.localScale = Vector3.one * value;
+
+        //reset
+        if(comparePoint >= 1)
+        {
+            isDelay = true;
+            p = dt = 0;
+        }
     }
 }

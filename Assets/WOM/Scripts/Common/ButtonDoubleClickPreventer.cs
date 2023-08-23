7bc9bdb3ffc0633dbf0af8ad6ac61a026c975010
freeboxwork <33273908+freeboxwork@users.noreply.github.com>
2023-08-24 00:05:30 +0900

    뽑기 버튼 연속 눌리는 버그 수정

---
+++ Assets/WOM/Scripts/Common/ButtonDoubleClickPreventer.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonDoubleClickPreventer : MonoBehaviour
+{
+    public float disableTime = 1.0f; // 버튼 비활성화 시간
+
+    private Button button;
+    private bool isButtonDisabled = false;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+    }
+
+    private void OnEnable()
+    {
+        button.onClick.AddListener(OnClickButton);
+    }
+
+    private void OnDisable()
+    {
+        button.onClick.RemoveListener(OnClickButton);
+    }
+
+    private void OnClickButton()
+    {
+        if (!isButtonDisabled)
+        {
+            isButtonDisabled = true;
+            button.interactable = false;
+            Invoke("EnableButton", disableTime);
+        }
+    }
+
+    private void EnableButton()
+    {
+        isButtonDisabled = false;
+        button.interactable = true;
+    }
+}
